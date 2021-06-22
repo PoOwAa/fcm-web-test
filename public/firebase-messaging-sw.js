597ae811145857bbfb557ee80fc7be653347f0b0
PoOwAa <raycsucsu@gmail.com>
2021-06-22 14:57:09 +0200

    a little refactor

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,27 +4,13 @@ importScripts('/__/firebase/init.js');
 
 const messaging = firebase.messaging();
 
-
-async function getToken() {
-    
-    try {
-        const token = await messaging.getToken({
-            vapidKey: 'BPgqekszJ3DKObBZbNEyRXJF9Q8jeWd8JIw0YFqNJKzJqE631o3ZSd_27ctH5AlVepgBgrL55yUfD5nco9M0Ss8',
-        });
-
-        console.log(token);
-    } catch(e) {
-        console.error(e);
-    }
-}
-
-async function listenNotification() {
+async function backgroundNotification() {
     messaging.onBackgroundMessage( (payload) => {
         console.log('fcm background message', payload);
 
-        const notificationTitle = 'Background message title';
+        const notificationTitle = 'SkyPark';
         const notificationOptions = {
-            body: 'Background message body.',
+            body: payload.data.default,
             icon: '/firebase-logo.png',
         };
 
@@ -32,5 +18,4 @@ async function listenNotification() {
     });
 }
 
-getToken();
-listenNotification();
+backgroundNotification();
